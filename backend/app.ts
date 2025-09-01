dc10f28d0bcd247542f230578466a15c7c6c1d5f
mlallai <matthieu.lallai@gmail.com>
2025-09-01 16:59:04 +0200

    add build stage and ts

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,11 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+
 const app = express();
 const port = 80;
 
 const welcometext = `App listening at http://localhost:${port} - Environment is: ${process.env.NODE_ENV} and Log Level is: ${process.env.LOG_LEVEL}`;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(welcometext);
 });
 

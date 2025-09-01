7157a7a41222230579276c6004dc9585e4516c02
mlallai <matthieu.lallai@gmail.com>
2025-09-01 15:47:22 +0200

    build-3 with configmap

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const welcometext = `App listening at http://localhost:${port} - Environment is: ${process.env.NODE_ENV} and Log Level is: ${process.env.LOG_LEVEL}`;
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(welcometext);
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(welcometext);
 });
